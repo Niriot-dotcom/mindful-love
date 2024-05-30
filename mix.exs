5b89c49a97b216c62213085b3ac3991d036e5f02
Paty Lopez <lopezpatyyarely@gmail.com>
2024-05-30 16:04:56 -0600

    add flatpickr dependency for calendar.

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule Mindfullove.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
+      {:number, "~> 1.0"},
       {:timex, "~> 3.7"},
       {:plug_cowboy, "~> 2.5"}
     ]
